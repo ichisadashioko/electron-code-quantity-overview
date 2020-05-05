5ebc8307fefd729d41fe03bc5a8a3e6cb6007995
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-05 19:24:07 +0900

    :zap: tray icon sample

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,23 @@
-import { app, BrowserWindow } from 'electron'
+import { app, Menu, Tray } from 'electron'
 import * as path from 'path'
-import * as url from 'url'
 
-function createWindow() {
-    // create the browser window
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        // set the background color to black
-        backgroundColor: '#111',
-        // set the title bar style
-        titleBarStyle: 'hiddenInset',
-        // don't show the window until it's ready, this prevents any white flickering
-        show: false,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
+let tray = null
+
+app.on('ready', function () {
+    tray = new Tray(path.join(__dirname, '../electron.png'))
+
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: <const>'radio' },
+        { label: 'Item2', type: <const>'radio', checked: true },
+        { label: 'Item3', type: <const>'radio' },
+        {
+            label: 'Exit', type: <const>'normal', click: function () {
+                // console.log(arguments)
+                app.exit()
+            }
         },
-    })
+    ])
 
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, '../index.html'),
-        protocol: 'file:',
-        slashes: true,
-    }))
-
-    mainWindow.once('ready-to-show', function () {
-        console.log(arguments)
-        mainWindow.show()
-    })
-
-    // open the DevTools
-    mainWindow.webContents.openDevTools()
-}
-
-// This method will be called when Electron has finished initialization
-// and is ready to create browser windows. Some APIs can only be used
-// after this event occurs.
-app.whenReady().then(createWindow)
-
-// quit when all windows are closed
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar to stay
-    // active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    tray.setToolTip('Electron tray icon')
+    tray.setContextMenu(contextMenu)
 })
-
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
-
-// In this file you can include the rest of your app's specific main
-// process code. You can also put them in separate files and require
-// them here.
